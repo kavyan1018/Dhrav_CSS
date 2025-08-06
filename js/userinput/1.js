69f69c63b64bf060468a76d8fe45bf515dbe9c88
Kavyan Nayak <kavyannayak104@gmail.com>
2025-08-06 14:49:04 +0530

    User input and display on browser

---
+++ js/userinput/1.js
@@ -0,0 +1,9 @@
+function displayInput(){
+
+    // get input from user
+    var input =  document.getElementById("fn").value;
+
+    // display the output
+    document.getElementById("output").innerText = input;
+
+}
